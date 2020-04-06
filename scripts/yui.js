d0d8f94b27b78df8b1afc8beaea8570c5bd7527d
nizidara <contact@nizidara.com>
2020-04-06 15:27:04 +0900

    yarn.lockからyuiを削除

--- scripts/yui.js
+++ scripts/yui.js
@@ -5,7 +5,7 @@
 //   ボット名 done     - TODO を完了にする
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表
+//   ボット名 donelist - 完了した TODO の一覧表示
 
 "use strict";
 //console.log("動け");
